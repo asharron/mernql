9b1203c81b8197e35bbcd278aedcafe3076cf530
Alexandrith Sharron <alexsharron@gmail.com>
2019-08-04 18:37:14 -0400

    Created mongodb connection

--- server/server.js
+++ server/server.js
@@ -1,21 +1,35 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
+const MongoClient = require('mongodb').MongoClient;
 const { buildSchema } = require('graphql');
-const { typeDefs }  = require('./graphql/schema.gql');
+const { typeDefs } = require('./graphql/schema.gql');
 const { resolverRoot } = require('./graphql/resolvers');
 
 const app = express();
 const port = 3000;
+const mongo_uri = 'mongodb://localhost:27017';
+
+var db = null;
 
 // Construct a schema, using GraphQL schema language
 var schema = buildSchema(typeDefs);
 
-app.use('/api', graphqlHTTP({
-  schema: schema,
-  rootValue: resolverRoot,
-  graphiql: true,
+app.use('/graphql', graphqlHTTP({
+    schema: schema,
+    rootValue: resolverRoot,
+    graphiql: true,
 }));
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => {
+    console.log(db);
+    res.send('Hello World!');
+});
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+MongoClient.connect(mongo_uri, { useNewUrlParser: true })
+    .then(client => {
+        db = client.db('my-db');
+        app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+    }).catch(error => {
+        console.error(error);
+    });
