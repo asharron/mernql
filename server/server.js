c4bd1debb2c2f7294f87550a5d775a0b3f3e3aed
Alexandrith Sharron <alexsharron@gmail.com>
2019-08-18 13:32:44 -0400

    Migration to GitLab

--- server/server.js
+++ server/server.js
@@ -1,35 +1,21 @@
-const express = require('express');
-const graphqlHTTP = require('express-graphql');
-const MongoClient = require('mongodb').MongoClient;
-const { buildSchema } = require('graphql');
+const { GraphQLServer } = require('graphql-yoga');
 const { typeDefs } = require('./graphql/schema.gql');
-const { resolverRoot } = require('./graphql/resolvers');
+const { prisma } = require('./graphql/generated/prisma-client');
 
-const app = express();
-const port = 8080;
-const mongo_uri = 'mongodb://localhost:27017';
+// 2
+const resolvers = {
+  Query: {
+      users: async (parent, args, context, info) => {
+          const users = await prisma.users();
+          return users;
+      }
+  }
+};
 
-var db = null;
-
-// Construct a schema, using GraphQL schema language
-var schema = buildSchema(typeDefs);
-
-app.use('/graphql', graphqlHTTP({
-    schema: schema,
-    rootValue: resolverRoot,
-    graphiql: true,
-}));
-
-app.get('/', (req, res) => {
-    console.log(db);
-    res.send('Hello World!');
+// 3
+const server = new GraphQLServer({
+  typeDefs,
+  resolvers,
 });
 
-MongoClient.connect(mongo_uri, { useNewUrlParser: true })
-    .then(client => {
-        db = client.db('my-db');
-        app.listen(port, () => console.log(`Example app listening on port ${port}!`));
-
-    }).catch(error => {
-        console.error(error);
-    });
+server.start(() => console.log(`Server is running on http://localhost:4000`));
